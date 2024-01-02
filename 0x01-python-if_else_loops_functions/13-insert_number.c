40fe3ca01be17dbc354f3517d5cc1103da60b460
Luke Herbst <lukeherbst37@gmail.com>
2024-01-03 01:49:43 +0200

    update lists.h to include stdlib.h to allow access to NULL

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,9 +8,11 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 	tmp = *head;
 	while (tmp != NULL)
+	{
 		if (number >= tmp->n)
 			break;
 		tmp = tmp->next;
+	}
 	if (tmp == NULL)
 	{
 		tmp = malloc(sizeof(listint_t));

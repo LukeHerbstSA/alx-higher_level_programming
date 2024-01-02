3062891d82350910fdd00fbcf9920b9f3ad5deb4
Luke Herbst <lukeherbst37@gmail.com>
2024-01-03 01:26:54 +0200

    fixed file 12 to match output - removed newline after every print and loop now starts 1 later

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(0, 101):
+    for i in range(1, 101):
         if (i % 3) == 0:
-            print("Fizz")
+            print("Fizz", end="")
         if (i % 5) == 0:
-            print("Buzz")
+            print("Buzz", end="")
         if (i % 3) != 0 and (i % 5) != 0:
-            print(i)
-        print(" ")
+            print(i, end="")
+        print(" ", end="")

67ebd5686a382a0b07fd50977461c88e0cd7b3bd
Luke Herbst <lukeherbst37@gmail.com>
2024-01-04 17:06:52 +0200

    test

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,4 +8,3 @@ if __name__ == "__main__":
     print("{} -".format(a), "{} =".format(b), "{}".format(sub(a, b)))
     print("{} x".format(a), "{} =".format(b), "{}".format(mul(a, b)))
     print("{} /".format(a), "{} =".format(b), "{}".format(div(a, b)))
-
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     i = len(sys.argv)
-
     print(i, end="")
-    if (i == 0 or i > 1)
-        print("arguments", end=="")
-    else
+    if (i == 0 or i > 1):
+        print("arguments", end="")
+    else:
         print("argument", end="")
-    if (i == 0)
+    if (i == 0):
         print(".")
-    else
+    else:
         print(":")
     for j in range(1, i):
         print("{}:".format(j), "{}".format(sys.argv[j]))

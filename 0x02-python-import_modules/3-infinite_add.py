6b72255c71475aef2ae41335fc4316930e41498e
root <lukeherbst37@gmail.com>
2024-01-04 08:43:14 -0800

    sum printed rather than returned

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    import sys
     i = len(sys.argv)
     sum = 0
 
     for j in range(1, i):
         sum = sum + int(sys.argv[j])
-    return (sum)
+    print(sum)

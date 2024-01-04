c9f555957e7dcb59f88a25d3791bb8500c93ac2f
Luke Herbst <lukeherbst37@gmail.com>
2024-01-04 17:56:47 +0200

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     i = len(sys.argv)
-    print(i, end="")
+    print(i, end=" ")
     if (i == 0 or i > 1):
         print("arguments", end="")
     else:
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     else:
         print(":")
     for j in range(1, i):
-        print("{}:".format(j), "{}".format(sys.argv[j]))
+        print(j + ":", "{}".format(sys.argv[j]))

f239f18762fa9e7e4f680120f1b37135cf9fa659
Luke Herbst <lukeherbst37@gmail.com>
2024-01-04 17:35:17 +0200

    fixed wrong sign use in string in print

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     b = 5
     print("{:d} +".format(a), "{:d} =".format(b), "{:d}".format(add(a, b)))
     print("{:d} -".format(a), "{:d} =".format(b), "{:d}".format(sub(a, b)))
-    print("{:d} x".format(a), "{:d} =".format(b), "{:d}".format(mul(a, b)))
+    print("{:d} *".format(a), "{:d} =".format(b), "{:d}".format(mul(a, b)))
     print("{:d} /".format(a), "{:d} =".format(b), "{:d}".format(div(a, b)))

14922aa744fbb4bae299beb2d3cdd0fa5117734e
root <lukeherbst37@gmail.com>
2024-01-06 06:36:50 -0800

    file no longer stops 1 short of end

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
     bad_chars = ["c", "C"]
     strcopy = my_string.copy()
     j = len(strcopy)
-    for i in range(-1, j * -1, -1):
+    for i in range(-1, j * -1 - 1, -1):
         for char in bad_chars:
             if (strcopy[i] == char):
                 strcopy.remove(char)

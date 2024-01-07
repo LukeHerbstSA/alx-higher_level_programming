159669ce71fb3ad2119dff4d5b954783961be20d
root <lukeherbst37@gmail.com>
2024-01-07 07:27:35 -0800

    str object now adds letters to end instead of attempting to access index

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,6 +6,6 @@ def no_c(my_string):
     j = 0
     for i in range(0, len(my_string)):
         if (my_string[i] not in bad_chars):
-            newstring[j] = my_string[i]
+            newstring += my_string[i]
             j = j + 1
     return (newstring)

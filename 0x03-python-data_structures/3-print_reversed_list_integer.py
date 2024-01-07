164a8df6cbe3982773aaafff799b5aa109921d3d
root <lukeherbst37@gmail.com>
2024-01-07 05:48:17 -0800

    testing if None check is required for my_list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if (my_list == "None" or my_list == False):
+        return (None)
     for i in range(-1, len(my_list) * -1 - 1, -1):
         print("{:d}".format(my_list[i]))

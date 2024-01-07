1253062240ad65b8851d2b712fc64036d2c6082d
root <lukeherbst37@gmail.com>
2024-01-07 08:10:34 -0800

    variable fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,6 +6,6 @@ def max_integer(my_list=[]):
     if (i == 0):
         return (None)
     for number in my_list:
-        if (number > max):
+        if (number > biggest):
             biggest = number
     return (biggest)

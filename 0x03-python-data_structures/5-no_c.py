1e9af4d327e8e1507d3583db2c1e5601dc5cfe31
root <lukeherbst37@gmail.com>
2024-01-07 06:56:36 -0800

    rewrite 5 to loop over bad chars first rather than list

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,9 +3,7 @@
 def no_c(my_string):
     bad_chars = ["c", "C"]
     strcopy = my_string.copy()
-    j = len(strcopy)
-    for i in range(-1, j * -1 - 1, -1):
-        for char in bad_chars:
-            if (strcopy[i] == char):
-                strcopy.remove(char)
+    for char in bad_chars:
+        while (char in strcopy):
+            strcopy.remove(char)
     return (strcopy)

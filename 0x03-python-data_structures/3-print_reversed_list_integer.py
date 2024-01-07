e188b474ebf9c7c4a8ddce82e7720b02cd4a4194
root <lukeherbst37@gmail.com>
2024-01-07 05:53:42 -0800

    fixed None comparison

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     j = len(my_list)
-    if (j == None or j == 0):
+    if (j is None or j == 0):
         return (None)
     for i in range(-1, j * -1 - 1, -1):
         print("{:d}".format(my_list[i]))

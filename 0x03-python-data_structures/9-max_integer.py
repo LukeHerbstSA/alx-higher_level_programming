2e3206ae0d474eae03e4a5923bb0d345a2846093
root <lukeherbst37@gmail.com>
2024-01-08 06:49:57 -0800

    return none before checking for length

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
+    if (my_list is None):
+        return (None)
     i = len(my_list)
     biggest = 0
-    if (i == 0 or my_list is None):
+    if (i == 0):
         return (None)
     for number in my_list:
         if (number > biggest):

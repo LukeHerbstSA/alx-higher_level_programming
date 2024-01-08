85b558ed375754767042f47ea474f426f4e6bbfe
root <lukeherbst37@gmail.com>
2024-01-08 03:37:23 -0800

    script now checks if one of the elements is None

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,9 +8,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     a_len = len(tuple_a)
     b_len = len(tuple_b)
-    val0 = 0 if a_len == 0 else tuple_a[0]
-    val1 = 0 if b_len == 0 else tuple_b[0]
-    val2 = 0 if a_len == 1 else tuple_a[1]
-    val3 = 0 if b_len == 1 else tuple_b[1]
+    val0 = 0 if a_len == 0 or tuple_a[0] is None else tuple_a[0]
+    val1 = 0 if b_len == 0 or tuple_b[0] is None else tuple_b[0]
+    val2 = 0 if a_len == 1 or tuple_a[1] is None else tuple_a[1]
+    val3 = 0 if b_len == 1 or tuple_b[1] is None else tuple_b[1]
     new_tuple = (val0 + val1, val2 + val3)
     return (new_tuple)

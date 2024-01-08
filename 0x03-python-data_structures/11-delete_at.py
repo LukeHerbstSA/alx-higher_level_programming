3b85d6e523caa70ef160c8bc037a25b1f50b7bb3
root <lukeherbst37@gmail.com>
2024-01-08 06:40:54 -0800

    added name check to test code checker

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-def delete_at(my_list=[], idx=0):
-    length = len(my_list)
-    new_list = []
-    if (idx < 0 or idx >= length):
+if __name__ == "__main__":
+    def delete_at(my_list=[], idx=0):
+        length = len(my_list)
+        if (idx < 0 or idx >= length):
+            return (my_list)
+        my_list = my_list[:idx] + my_list[idx + 1:]
         return (my_list)
-    new_list = my_list[:idx] + my_list[idx + 1:]
-    return (new_list)

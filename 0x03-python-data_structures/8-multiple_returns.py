b3afdb86d332cb88021b5dbb50e2adec253bc305
root <lukeherbst37@gmail.com>
2024-01-08 04:00:40 -0800

    added solution to 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    
+    char = None if length == 0 else sentence[0]
+    new_tuple = (length, char)
+    return (new_tuple)

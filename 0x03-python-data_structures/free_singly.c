06f3707c56a157353b760b94ce9ed7c77bb81589
root <lukeherbst37@gmail.com>
2024-01-09 05:51:10 -0800

    fixed syntax

--- 0x03-python-data_structures/free_singly.c
+++ 0x03-python-data_structures/free_singly.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 
-def free_singly(listint_t **reversetmp)
+/**
+	* free_singly - void func
+	* Description: frees passed singly
+	* @reversetmp: passed singly
+	*/
+void free_singly(listint_t **reversetmp)
 {
 	listint_t prevnode = NULL;
 	while (reversetmp != NULL)

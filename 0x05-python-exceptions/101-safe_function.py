285dfdbf6383a43248f0f4500af159df4ad7ed6f
Luke Herbst <lukeherbst37@gmail.com>
2024-01-23 00:03:15 +0200

    first solution to 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,4 +1,10 @@
 #!/usr/in/python3
 
 def safe_function(fct, *args):
-    return
+    result = None
+
+    try:
+        result = fct(args[0], args[1])
+    except Exception as ex:
+        print("Exception: ", ex)
+    return (result)

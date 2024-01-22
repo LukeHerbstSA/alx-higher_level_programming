99def3b530dd4a75caa63a61cc4f08a27dbd26cd
Luke Herbst <lukeherbst37@gmail.com>
2024-01-22 22:52:01 +0200

    rewrote task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,11 +5,12 @@ def safe_print_list(my_list=[], x=0):
 
     if (x == 0 or x < 0 or my_list is None):
         return (y)
-    for i in range(0, x):
-        try:
-            print("{}".format(my_list[i]), end="")
+    try:
+        for i in range(0, x):
+            print(my_list[i], end="")
             y += 1
-        except IndexError:
-            break
-    print()
+    except IndexError:
+        return (y)
+    finally:
+        print()
     return (y)

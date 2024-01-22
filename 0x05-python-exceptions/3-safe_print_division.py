3e1cc2f94168998b8192ce59569ba7f8dea8437e
Luke Herbst <lukeherbst37@gmail.com>
2024-01-22 23:19:17 +0200

    fixed finally statement in task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,8 +8,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         sum = None
     finally:
-        if sum is not None:
-            print("Inside Result: {:.1f}".format(sum))
-        else:
-            print("Inside Result: {}".format(None))
+        print("Inside Result: {}".format(sum))
     return (sum)

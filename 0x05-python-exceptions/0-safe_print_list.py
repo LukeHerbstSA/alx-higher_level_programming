33ab48ff2415b8a3ffb171846a3cfa089245e17b
Luke Herbst <lukeherbst37@gmail.com>
2024-01-22 19:41:25 +0200

    testing superficial changes made to function

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,16 +5,11 @@ def safe_print_list(my_list=[], x=0):
 
     if (x == 0 or x < 0 or my_list is None):
         return (y)
-    try:
-        for element in my_list:
-            if (y == x):
-                break
-            print(element, end="")
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
             y += 1
-        if (y < x):
-            raise IndexError
-    except IndexError:
-        print()
-        return (y)
+        except:
+            break
     print()
     return (y)

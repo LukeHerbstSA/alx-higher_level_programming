80266f7de07e67c4f222a330facaee280c259aa8
Luke Herbst <lukeherbst37@gmail.com>
2024-01-23 23:10:09 +0200

    changed print to print empty line instead of newline when size = 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,7 +29,7 @@ class Square:
 
     def my_print(self):
         if (self.__size == 0):
-            print()
+            print("")
         for var in range(0, self.position[1]):
             print()
         for i in range(0, self.__size):

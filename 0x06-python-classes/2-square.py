1151588fb824642077fb4de7c31902bea389b037
Luke Herbst <lukeherbst37@gmail.com>
2024-01-23 18:05:34 +0200

    testing what amount of docstrings is required

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
+"""module with only one class, Square
+"""
 class Square:
+    """class square with one function init
+    """
     def __init__(self, size=0):
         if (not isinstance(size, int)):
             print("size must be an integer")

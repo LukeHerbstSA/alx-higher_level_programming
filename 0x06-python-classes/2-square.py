c769958199718dbbb2be92fc883d65fa06656dab
Luke Herbst <lukeherbst37@gmail.com>
2024-01-23 18:16:31 +0200

    error msgs are passed into raised exceptionsw

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,9 +8,7 @@ class Square:
     """
     def __init__(self, size=0):
         if (not isinstance(size, int)):
-            print("size must be an integer")
-            raise TypeError
+            raise TypeError("size must be an integer")
         if (size < 0):
-            print("size must be >= 0")
-            raise ValueError
+            raise ValueError("size must be >= 0")
         self.__size = size

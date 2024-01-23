c0149b757afc51e8ed38b04b48c596724c74fd4b
Luke Herbst <lukeherbst37@gmail.com>
2024-01-23 17:52:53 +0200

    testing checker

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
+"""I hope this unnecessary docstring passes the checker
+"""
 class Square:
-    """
-    An empty class called square used to demonstrate class creation
+    """An empty class called square used to demonstrate class creation
     """

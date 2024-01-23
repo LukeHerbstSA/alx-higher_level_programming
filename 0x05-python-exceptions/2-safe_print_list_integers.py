885fb6953e70cea1a0056c26fc86848d21622856
Luke Herbst <lukeherbst37@gmail.com>
2024-01-24 00:39:22 +0200

    testing returns

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     y = 0
 
     if (x == 0 or x < 0 or not my_list):
-        return (y)
+        return (0)
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")

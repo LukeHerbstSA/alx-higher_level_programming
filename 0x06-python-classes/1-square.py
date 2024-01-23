3d3393f0ea2e825e8f5275e5d41e3a72860e99ab
Luke Herbst <lukeherbst37@gmail.com>
2024-01-23 18:03:37 +0200

    testing if init variables require docstrings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+""" modules with one class, Square
+"""
+
 
 class Square:
+    """ defines instances of class Square
+    """
     def __init__(self, size):
+        """ initializer function for instantiations of Square
+
+        Args:
+            param1 (int): passed size of Square
+        """
         self.__size = size

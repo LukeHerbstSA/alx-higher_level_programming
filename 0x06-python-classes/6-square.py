9bcbd8fecea92096c64251a6fc93a0ae7ffbbfac
Luke Herbst <lukeherbst37@gmail.com>
2024-01-23 22:25:47 +0200

    changed my_print to now print a number of spaces correlation to position[0]

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,9 +31,11 @@ class Square:
     def my_print(self):
         if (self.__size == 0):
             print("")
+        for l in range(0, self.position[1]):
+            print()
         for i in range(0, self.__size):
             for x in range(0, self.position[0]):
-                print("_", end="")
+                print(" ", end="")
             for j in range(0, self.__size):
                 print("#", end="")
             print()

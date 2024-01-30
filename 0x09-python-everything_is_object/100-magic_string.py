f622d419499c4e5b116ca13877685d1291a08860
Luke Herbst <lukeherbst37@gmail.com>
2024-01-30 18:19:11 +0200

    pushing to test files on linux

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+iteration = 0
+def magic_string(iteration++)
+    return (result = ("BestSchool, " * iteration) if iteration > 1 else "BestSchool")
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,1 @@
+Answers to several alx theory questions (viewers wont be able to see the question the answer is related to)

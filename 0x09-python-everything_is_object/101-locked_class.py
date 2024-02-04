52d3a903d95b193b651fd0919183b94e626388cd
Luke Herbst <lukeherbst37@gmail.com>
2024-02-04 19:34:04 +0200

    fixed syntax error

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,7 +9,7 @@ class LockedClass:
     """
     locked class that prevents new instance attributes being added
     """
-    def __slots__("first_name")
+    def __slots__ = ("first_name")
 
     def __init__(self, name):
         self.first_name = name

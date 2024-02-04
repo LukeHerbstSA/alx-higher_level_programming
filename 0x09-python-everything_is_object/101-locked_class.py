398fadde53120b0a06b5b63c21b6ceb9f04ab024
Luke Herbst <lukeherbst37@gmail.com>
2024-02-04 19:39:45 +0200

    extra pedantic documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,14 +2,24 @@
 
 """
 simple locked class
+
+Example usage:
+
+    obj = LockedClass("james")
+    obj.first_name = "franco"
 """
 
 
 class LockedClass:
     """
     locked class that prevents new instance attributes being added
+
+    only allows attribute first name to be changed
     """
-    def __slots__ = ("first_name")
+    def __slots__ = "first_name"
 
     def __init__(self, name):
+        """
+        pedantic init documentation
+        """
         self.first_name = name

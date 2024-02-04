a061f5b551bdd163438a86d7e88f59af8b9f3850
Luke Herbst <lukeherbst37@gmail.com>
2024-02-04 19:24:04 +0200

    fixed style issues

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,5 +9,6 @@ class LockedClass:
     locked class that prevents new instance attributes being added
     """
     def __slots__("first_name")
+
     def __init__(self, name):
         self.first_name = name

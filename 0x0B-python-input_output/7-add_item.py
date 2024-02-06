124ebfb03f411a94fbb8d54f21d8ab47ac3902b1
Luke Herbst <lukeherbst37@gmail.com>
2024-02-06 22:16:44 +0200

    added a if main check

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,3 +15,5 @@ def filewriter():
         mylist.append(sys.argv[i])
     with open("add_item.json", "w+", encoding="utf-8") as myfile:
         json.dump(mylist, myfile)
+if __name__ == "__main__":
+    filewriter()

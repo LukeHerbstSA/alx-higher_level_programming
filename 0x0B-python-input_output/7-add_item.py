843bbb7a429634c166e39261ad6fb13cb150cc79
Luke Herbst <lukeherbst37@gmail.com>
2024-02-06 17:26:14 +0200

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,9 +7,12 @@ from load_from_json_file import load_from_json_file
 import sys
 import json
 
-mylist = []
 
-for i in range(1, len(sys.argv)):
-    mylist.append(sys.argv[i])
-with open("add_item.json", "w+", encoding="utf-8") as myfile:
-    json.dump(mylist, myfile)
+def filewriter():
+    mylist = []
+
+    for i in range(1, len(sys.argv)):
+        mylist.append(sys.argv[i])
+    with open("add_item.json", "w+", encoding="utf-8") as myfile:
+        json.dump(mylist, myfile)
+filewriter()

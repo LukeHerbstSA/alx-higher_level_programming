34ace479b036cf098f29787661eb7f4b0a16ac75
Luke Herbst <lukeherbst37@gmail.com>
2024-02-06 17:16:31 +0200

    first submit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+pedantic pedantic pedantic documentation
+"""
 from save_to_json_file.py import save_to_json_file
 from load_from_json_file import 6-load_from_json_file.py
-def 
+import sys
+import json
+
+mylist = []
+
+for i in range(1, len(sys.argv)):
+    mylist.append(sys.argv[i])
+mylist = json.dumps(mylist)
+with open("add_item.json", "w+", encoding="utf-8") as myfile:
+    myfile.write(mylist)

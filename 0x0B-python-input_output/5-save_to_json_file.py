48aa534133206f945595cc1caf618d1e70058a00
Luke Herbst <lukeherbst37@gmail.com>
2024-02-06 16:04:27 +0200

    testing broken alx checker

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+pedantic pedantic pedantic documentation
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    changes my_obj to json version then writes to filename
+    """
+    newobj = json.dumps(my_obj)
+    with open(filename, "w", encoding="utf-8") as myfile:
+        myfile.write(newobj)

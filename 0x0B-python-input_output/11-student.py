ff17e39735bea4b13d8fed95435373db24f0e698
Luke Herbst <lukeherbst37@gmail.com>
2024-02-07 00:24:53 +0200

    first submit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+pedantic pedantic pedantic documentation
+"""
+
+
+class Student:
+    """
+    student class with init and to_json functionality
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (isinstance(attrs, list) and len(attrs) > 0):
+            mydict = vars(self)
+            newdict = {}
+            for key in mydict:
+                if (key in attrs):
+                    newdict[key] = mydict[key]
+            return (newdict)
+        else:
+            return (vars(self))
+
+    def reload_from_json(self, json):
+        for key in json:
+            setattr(self, key, value)

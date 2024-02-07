71da5098647ead2e6d8e85f4ba34a60f8ae690f0
Luke Herbst <lukeherbst37@gmail.com>
2024-02-07 16:14:48 +0200

    first submit

---
+++ 0x0A-python-inheritance/10-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+pedantic pedantic pedantic documentation
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    pedantic pedantic pedantic documentation
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+    def area(self):
+        return (super().area())

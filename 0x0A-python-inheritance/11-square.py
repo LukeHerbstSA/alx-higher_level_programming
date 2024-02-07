712f33b421e6b568de4969edd1c1e3ea53bff6a4
Luke Herbst <lukeherbst37@gmail.com>
2024-02-07 16:31:27 +0200

    first submit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+pedantic pedantic pedantic documentation
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    pedantic pedantic pedantic documentation
+    """
+    def __str__(self):
+        print("[Square] {}/{}".format(self.__size, self.__size))
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+    def area(self):
+        return (size * size)
+

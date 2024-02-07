1422ea3047e32e4710f5893a6321c3e28fa2fa6b
Luke Herbst <lukeherbst37@gmail.com>
2024-02-07 14:59:55 +0200

    testing

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,7 +8,7 @@ def inherits_from(obj, a_class):
     """
     checks if obj is indirectly or directly sub of a_class
     """
-    if (not isinstance(obj, a_class) and issubclass(type(obj), a_class)):
+    if (issubclass(type(obj), a_class)):
         return (True)
     else:
         return (False)

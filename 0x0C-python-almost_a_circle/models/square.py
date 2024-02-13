d7a2d01d43032a15c550d8bca72cd7c19f8f1927
root <lukeherbst37@gmail.com>
2024-02-13 11:45:03 -0800

    testing

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,16 +10,25 @@ class Square(Rectangle):
     class square uses inheritance to initialize special isntances
     """
     def __str__(self):
+        """
+        returns the string repr of an instance
+        """
         firsthlf = "[Square] ({}) {}/{} ".format(self.id, self.x, self.y)
         scndhlf = "- {}".format(self.width)
         return (firsthlf + scndhlf)
 
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        initializes square isntances
+        """
         width = size
         height = size
         super().__init__(width, height, x, y, id)
 
     def update(self, *args, **kwargs):
+        """
+        updates current instance with the passed dicts / lists
+        """
         if (args is None):
             for key, value in kwargs:
                 setattr(self, key, value)
@@ -34,14 +43,23 @@ class Square(Rectangle):
                 self.y = args[i]
 
     def to_dictionary(self):
+        """
+        returns the dictionary repr of writable attributes
+        """
         return (self.__dict__)
 
     @property
     def size(self):
+        """
+        returns the width of the instance
+        """
         return (self.width)
 
     @size.setter
     def size(self, value):
+        """
+        sets the size of the instance
+        """
         super().validator("width", value)
         self.width = value
         self.height = value

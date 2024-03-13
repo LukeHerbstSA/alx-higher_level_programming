2ed767106780c0a8043bcf528e3950bb2ffc25e5
Luke <lukeherbst37@gmail.com>
2024-03-13 23:34:58 +0200

    first submit

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all cities in database table
-SELECT * FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC, state_id ASC, name;
+SELECT id, state_id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC, state_id ASC, name;

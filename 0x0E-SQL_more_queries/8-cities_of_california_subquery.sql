f7f290b0bee22709743fd82201a75a53916184e8
Luke <lukeherbst37@gmail.com>
2024-03-13 23:36:59 +0200

    removed displaying of state_id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all cities in database table
-SELECT id, state_id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC, state_id ASC, name;
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC, name;

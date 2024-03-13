ae8defee2dd255308ccba868a5a6bba94ed5ad12
Luke <lukeherbst37@gmail.com>
2024-03-13 21:41:08 +0200

    changed to switch to database first (obviously)

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- creates database table with columns
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	id INT NOT NULL AUTO_INCREMENT UNIQUE,
 	state_id INT NOT NULL,

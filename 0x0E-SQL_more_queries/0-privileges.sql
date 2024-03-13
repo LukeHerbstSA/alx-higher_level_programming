172958fb7662d8153992ce76c31b9d29a76b5566
Luke <lukeherbst37@gmail.com>
2024-03-13 19:21:08 +0200

    added server name to test

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists privileges of mysql users on current server
-SHOW GRANTS FOR user_0d_1;
-SHOW GRANTS FOR user_0d_2;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;

70e366c3987ce8d661fb5b21333497d9af2e5535
Luke <lukeherbst37@gmail.com>
2024-03-13 21:39:14 +0200

    changed syntax to match

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,8 @@
 -- creates database table with columns
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-	id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT UNIQUE,
 	state_id INT NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id),
-	name VARCHAR(256) NOT NULL);
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id));

04d0454aa38b0a9f3dc31d655f487303ffbde5c4
Luke <lukeherbst37@gmail.com>
2024-03-14 14:46:49 +0200

    first submits -full perms

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,5 @@
--- selects shows from database including NULL genres
+-- shows records with no genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
